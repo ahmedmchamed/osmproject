c892a064e181bf6957da9aa922d4fc3cdad4ba7e
ahmedmchamed <tunashark@gmail.com>
2020-09-23 19:45:09 +0100

    basic board file

---
+++ board.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
+
+void PrintBoard(const vector<vector<int>> &board) {
+    for(const auto &row : board) {
+        for(const auto &number : row) {
+            cout << number;
+        }
+        cout << std::endl;
+    }
+}
+
+int main() {
+
+    vector<vector<int>> board{
+        {0, 1, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0}
+    };
+
+    PrintBoard(board);
+
+}
