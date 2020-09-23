a485e603c5fb2bb31c0e4de0e2f69b0f52a9c7a9
ahmedmchamed <tunashark@gmail.com>
2020-09-23 23:01:50 +0100

    gitignore and parseline function

--- board.cpp
+++ board.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
 
 using std::cout;
 using std::string;
 using std::vector;
 using std::ifstream;
+using std::istringstream;
 
 namespace fileName {
     string boardFileName = "1.board";
 }
 
+vector<int> ParseLine(string line) {
+    istringstream lineToParse(line);
+
+    vector<int> ivec;
+
+    int number = 0;
+    char comma = '0';
+
+    while(lineToParse >> number >> comma && comma == ',') {
+        ivec.push_back(number);
+    }
+
+    return ivec;
+}
+
 void ReadBoardFile() {
     ifstream boardFile;
     boardFile.open(fileName::boardFileName);
+    vector<int> ivec;
 
     if (boardFile) {
         cout << "Board file successfully opened" << std::endl;
         string line;
         while(getline(boardFile, line)) {
-            cout << line << std::endl;
+            // cout << line << std::endl;
+            ivec = ParseLine(line);
         }
+    }
+
+    for (auto i : ivec) {
+        cout << i << std::endl;
     }
 }
 
@@ -45,5 +68,5 @@ int main() {
 
     PrintBoard(board);
 
-ReadBoardFile();
+    ReadBoardFile();
 }
